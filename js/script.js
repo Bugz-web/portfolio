9ec05208908fa71f3853a93469d2cb2d9929cd85
Anu Adegbite <weidergeboren90@gmail.com>
2026-07-13 20:45:12 +0100

    Add the typing text script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // Add custom JavaScript here
+
+function runTypeText() {
+    const textElement = document.getElementById("typing-text");
+    const text= "I am Anu Adegbite";
+    const text_delay = 100;
+
+    typerText(textElement,text,text_delay);
+}
+
+
+function typerText(textElement, text, delay) {
+    for(let i=0; i < text.length; i++) {
+        setTimeout(() => {
+            textElement.textContent += text.charAt(i);
+        }, i*delay);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", runTypeText);
